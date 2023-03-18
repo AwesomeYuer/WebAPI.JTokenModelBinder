2e654880b13a8ab109a10cc1bb0b791be9cad80f
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-03-18 22:58:09 +0800

    1

--- WebApplication1/Controllers/EchoController.JTokenModelBinder.cs
+++ WebApplication1/Controllers/EchoController.JTokenModelBinder.cs
@@ -1,160 +1,158 @@
-namespace WebApplication1.Controllers
+namespace WebApplication1.Controllers;
+
+using Microshaoft;
+using Microshaoft.Web;
+using Microshaoft.WebApi.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+//[ConstrainedRoute("api/[controller]")]
+//[ApiController]
+//[Route("api/[controller]")]
+public partial class AdminController : ControllerBase
 {
-    using Microshaoft;
-    using Microshaoft.Web;
-    using Microshaoft.WebApi.ModelBinders;
-    using Microsoft.AspNetCore.Mvc;
-    using Newtonsoft.Json.Linq;
-    using System.Text.Json.Nodes;
+    //private readonly ILogger<AdminController> _logger;
 
-    //[ConstrainedRoute("api/[controller]")]
-    //[ApiController]
-    //[Route("api/[controller]")]
-    public partial class AdminController : ControllerBase
+    //public AdminController(ILogger<AdminController> logger)
+    //{
+    //    _logger = logger;
+    //}
+
+    [HttpDelete]
+    [HttpGet]
+    [HttpHead]
+    [HttpOptions]
+    [HttpPatch]
+    [HttpPost]
+    [HttpPut]
+    [Route("Echo/JToken/{* }")]
+    public ActionResult EchoJToken
+            (
+                 [ModelBinder(typeof(JTokenModelBinder))]
+                    JToken parameters //= null!
+            )
     {
-        //private readonly ILogger<AdminController> _logger;
+        var (callerMemberName, callerFilePath, callerLineNumber) = CallerHelper.GetCallerInfo();
 
-        //public AdminController(ILogger<AdminController> logger)
-        //{
-        //    _logger = logger;
-        //}
-
-        [HttpDelete]
-        [HttpGet]
-        [HttpHead]
-        [HttpOptions]
-        [HttpPatch]
-        [HttpPost]
-        [HttpPut]
-        [Route("Echo/JToken/{* }")]
-        public ActionResult EchoJToken
-                (
-                     [ModelBinder(typeof(JTokenModelBinder))]
-                        JToken parameters //= null!
-                )
-        {
-            var (callerMemberName, callerFilePath, callerLineNumber) = CallerHelper.GetCallerInfo();
-
-            return
-                Request
-                    .EchoJsonRequestJsonResult<JToken>
-                        (
-                            parameters
-                            , new
+        return
+            Request
+                .EchoJsonRequestJsonResult<JToken>
+                    (
+                        parameters
+                        , new
+                        {
+                            Caller = new
                             {
-                                Caller = new
-                                {
-                                    callerMemberName
-                                    , callerFilePath
-                                    , callerLineNumber
-                                }
+                                callerMemberName
+                                , callerFilePath
+                                , callerLineNumber
                             }
-                        );
-        }
+                        }
+                    );
+    }
 
 
-        [HttpDelete]
-        [HttpGet]
-        [HttpHead]
-        [HttpOptions]
-        [HttpPatch]
-        [HttpPost]
-        [HttpPut]
-        [Route("Echo/JToken/{* }")]
-        public ActionResult EchoJToken()
-        {
-            var (callerMemberName, callerFilePath, callerLineNumber) = CallerHelper.GetCallerInfo();
+    [HttpDelete]
+    [HttpGet]
+    [HttpHead]
+    [HttpOptions]
+    [HttpPatch]
+    [HttpPost]
+    [HttpPut]
+    [Route("Echo/JToken/{* }")]
+    public ActionResult EchoJToken()
+    {
+        var (callerMemberName, callerFilePath, callerLineNumber) = CallerHelper.GetCallerInfo();
 
-            return
-                Request
-                    .EchoJsonRequestJsonResult<JToken>
-                        (
-                            null!
-                            , new
+        return
+            Request
+                .EchoJsonRequestJsonResult<JToken>
+                    (
+                        null!
+                        , new
+                        {
+                            Caller = new
                             {
-                                Caller = new
-                                {
-                                    callerMemberName
-                                    , callerFilePath
-                                    , callerLineNumber
-                                }
+                                callerMemberName
+                                , callerFilePath
+                                , callerLineNumber
                             }
+                        }
+                    );
+    }
+
+
+    [HttpDelete]
+    [HttpGet]
+    [HttpHead]
+    [HttpOptions]
+    [HttpPatch]
+    [HttpPost]
+    [HttpPut]
+    [Route("Echo/JToken/{* }")]
+    public async Task<ActionResult> EchoJTokenAsync
+            (
+                 [ModelBinder(typeof(JTokenModelBinder))]
+                    JToken parameters //= null!
+            )
+    {
+        var (callerMemberName, callerFilePath, callerLineNumber) = CallerHelper.GetCallerInfo();
+
+        return
+            await
+                Task
+                    .FromResult
+                        (
+                            Request
+                                .EchoJsonRequestJsonResult<JToken>
+                                    (
+                                        parameters
+                                        , new
+                                        {
+                                            Caller = new
+                                            {
+                                                callerMemberName
+                                                , callerFilePath
+                                                , callerLineNumber
+                                            }
+                                        }
+                                    )
                         );
-        }
 
+    }
 
-        [HttpDelete]
-        [HttpGet]
-        [HttpHead]
-        [HttpOptions]
-        [HttpPatch]
-        [HttpPost]
-        [HttpPut]
-        [Route("Echo/JToken/{* }")]
-        public async Task<ActionResult> EchoJTokenAsync
-                (
-                     [ModelBinder(typeof(JTokenModelBinder))]
-                        JToken parameters //= null!
-                )
-        {
-            var (callerMemberName, callerFilePath, callerLineNumber) = CallerHelper.GetCallerInfo();
+    [HttpDelete]
+    [HttpGet]
+    [HttpHead]
+    [HttpOptions]
+    [HttpPatch]
+    [HttpPost]
+    [HttpPut]
+    [Route("Echo/JToken/{* }")]
+    public async Task<ActionResult> EchoJTokenAsync()
+    {
+        var (callerMemberName, callerFilePath, callerLineNumber) = CallerHelper.GetCallerInfo();
 
-            return
-                await
-                    Task
-                        .FromResult
-                            (
-                                Request
-                                    .EchoJsonRequestJsonResult<JToken>
-                                        (
-                                            parameters
-                                            , new
+        return
+            await
+                Task
+                    .FromResult
+                        (
+                            Request
+                                .EchoJsonRequestJsonResult<JToken>
+                                    (
+                                        null!
+                                        , new
+                                        {
+                                            Caller = new
                                             {
-                                                Caller = new
-                                                {
-                                                    callerMemberName
-                                                    , callerFilePath
-                                                    , callerLineNumber
-                                                }
+                                                callerMemberName
+                                                , callerFilePath
+                                                , callerLineNumber
                                             }
-                                        )
-                            );
+                                        }
+                                    )
+                        );
 
-        }
- 
-        [HttpDelete]
-        [HttpGet]
-        [HttpHead]
-        [HttpOptions]
-        [HttpPatch]
-        [HttpPost]
-        [HttpPut]
-        [Route("Echo/JToken/{* }")]
-        public async Task<ActionResult> EchoJTokenAsync()
-        {
-            var (callerMemberName, callerFilePath, callerLineNumber) = CallerHelper.GetCallerInfo();
-
-            return
-                await
-                    Task
-                        .FromResult
-                            (
-                                Request
-                                    .EchoJsonRequestJsonResult<JToken>
-                                        (
-                                            null!
-                                            , new
-                                            {
-                                                Caller = new
-                                                {
-                                                    callerMemberName
-                                                    , callerFilePath
-                                                    , callerLineNumber
-                                                }
-                                            }
-                                        )
-                            );
-
-        }
     }
 }
